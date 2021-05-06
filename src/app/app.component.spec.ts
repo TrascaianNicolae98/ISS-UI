d8bae2307d4c75ef5a09a2d648f5351748cd2154
Acalinei Beniamin <beniacalinei1@yahoo.com>
2021-05-06 15:39:29 +0300

    "ProjectStructureOnFolders-Preparing the workspace by creating some components."

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the components', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Conference-UI app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Conference-UI components is running!');
   });
 });
